d1b21bf2cbbe060c3ff354e30c5221941f3d8269
Gabriel Luiz Carpes <gabrielluizcm@hotmail.com>
2023-01-17 10:53:15 -0300

    Fix on example reducer

--- src/store/modules/example/reducer.js
+++ src/store/modules/example/reducer.js
@@ -1,6 +1,7 @@
 const initialState = {};
 
-export default function exampleReducer(action, state = initialState) {
+// eslint-disable-next-line default-param-last
+export default function exampleReducer(state = initialState, action) {
   switch (action.type) {
     default: {
       return state;
